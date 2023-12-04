2964940f2f613942a777c03be6fca72791bb82ff
mukeshsarma054@gmail.com <mukeshsharma054@gmail.com>
2023-12-04 10:06:25 +0530

    serverless changes

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../generated/client';
 
 interface IDbClient {
   readonly client: PrismaClient;
